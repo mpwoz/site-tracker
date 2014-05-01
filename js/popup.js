c6b2860cda1db6fa88540c200e7c62c79664ddb8
mpwoz <martin.woz@gmail.com>
2014-05-01 12:57:48 -0500

    Sorting icons based on size, some refactoring

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,17 @@
-var addIcon = function(url, size, time) {
+var addIcon = function(picUrl, search) {
 	var image = $('<img>');
 
 	// set src and size
-	$(image).attr("src", url);
-	$(image).css("width", size * 50 + 30);
-	$(image).css("height", size * 50 + 30);
-	$(image).attr("title", time);
+	$(image).attr("src", picUrl);
+	$(image).css("width", search.scale * 50 + 30);
+	$(image).css("height", search.scale * 50 + 30);
+	$(image).attr("title", search.url + ": " + search.time + "ms");
 	$(image).addClass("tooltip");
 	// append to body
 	$('#content').append($(image));
 };
 
 isSearchAvailable = true;
-
-// addIcon("facebook.com", 1);
-// addIcon("google.com", 0.6);
-// addIcon("reddit.com", 1);
-// addIcon("illinois.edu", .4);
-// 
 
 google.load("search", "1");
 
@@ -26,29 +20,13 @@ function searchComplete() {
 	// Check that we got results
 	if (imageSearch.results && imageSearch.results.length > 0) {
 
-		// Grab our content div, clear it.
-		// var contentDiv = $('#content');
-
-
 		// Loop through our results, printing them to the page.
 		var result = imageSearch.results[0];
 
-		// var imgContainer = document.createElement('div');
-
-
-		// var newImg = document.createElement('img');
-		addIcon(result.url, search[index].scale, search[index].time +"ms");
+		addIcon(result.url, search[index]);
 		index = index + 1;
 
 		isSearchAvailable = true;
-
-		// There is also a result.url property which has the escaped version
-		// newImg.src = result.url;
-		// imgContainer.appendChild(newImg);
-
-		// // Put our title + image in the content
-		// contentDiv.append(imgContainer);
-
 
 	}
 }
@@ -63,84 +41,59 @@ $(function() {
 	// complete.  The imageSearch object will have results in it.
 	imageSearch.setSearchCompleteCallback(this, searchComplete, null);
 
-	// imageSearch.execute("facebook icon");
-	// isSearchAvailable=false;
-
 	index = 0;
 
-
-	// search = [{
-	// 	search: "facebook icon",
-	// 	scale: 1,
-	// 	time: ""
-	// }, {
-	// 	search: "google icon",
-	// 	scale: 0.05
-	// }, {
-	// 	search: "reddit icon",
-	// 	scale: 0.7
-	// }, {
-	// 	search: "twitter icon",
-	// 	scale: 0.3
-	// }, {
-	// 	search: "illinois icon",
-	// 	scale: 0.9
-	// }];
-
-
-
 	var getDataAndRender = function(val) {
-
-
-		if (val) {
-
-			// var keys = [];
-			// for (var k in val) keys.push(k);
-			//
-
-			// total up the time 
-			val=val.pages;
-			total = 0;
-			for (var k in val) {
-				total = total + parseInt(val[k]);
-			}
-
-			search = [];
-
-			for (var k in val) {
-
-				search.push({
-					search: k+ " icon",
-					time: val[k],
-					scale: parseInt(val[k]) / total
-
-				});
-			}
-
-
-
-			// var fun = function(array) {
-			// 	// var index=0;
-			// 	// 
-			var intervalfun = function() {
-				if (isSearchAvailable) {
-					if (index < search.length) {
-						imageSearch.execute(search[index].search);
-
-						isSearchAvailable = false;
-					} else {
-						//break out of intervarl
-						clearInterval(intervalID);
-						$('.tooltip').tooltipster();
-					}
-
-				}
-			}
-
-			intervalID = setInterval(intervalfun, 50);
-		} else {
+    if (!val) {
 			alert("no data retrived from local storage");
-		}
+      return;
+    }
+
+    // Total time spent on all pages (to divide by for relative scaling)
+    val=val.pages;
+    total = 0;
+    for (var k in val) {
+      total = total + parseInt(val[k]);
+    }
+
+    search = [];
+
+    // The queries to search for icons, and relative scales
+    for (var k in val) {
+      search.push({
+        search: k+ " icon",
+        time: val[k],
+        scale: parseInt(val[k]) / total,
+        url: k
+      });
+    }
+
+
+    var compareTimes = function(a,b) {
+      if (a.time < b.time)
+         return -1;
+      if (a.time > b.time)
+        return 1;
+      return 0;
+    };
+    search.sort(compareTimes);
+
+
+
+    var intervalfun = function() {
+      if (isSearchAvailable) {
+        if (index < search.length) {
+          imageSearch.execute(search[index].search);
+          isSearchAvailable = false;
+        } else {
+          //break out of intervarl
+          clearInterval(intervalID);
+          $('.tooltip').tooltipster();
+        }
+      }
+    }
+
+    intervalID = setInterval(intervalfun, 50);
 	};
 
 	chrome.storage.local.get("pages", getDataAndRender);
