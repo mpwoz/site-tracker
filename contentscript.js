63521232e2854092fe72820466516bafeedce5e8
mpwoz <martin.woz@gmail.com>
2014-04-30 19:07:39 -0500

    rename script

---
+++ contentscript.js
@@ -0,0 +1,21 @@
+var port = chrome.runtime.connect();
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "FROM_PAGE")) {
+    console.log("Content script received: " + event.data.text);
+    port.postMessage(event.data.text);
+  }
+}, false);
+
+
+/* 
+// This is an example script on the webpage
+document.getElementById("theButton").addEventListener("click",
+    function() {
+  window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
+}, false);
+*/
