198ab9751fbe9b874e5c12b872dac54c2fc344ce
mpwoz <martin.woz@gmail.com>
2014-04-30 19:07:25 -0500

    Updating content script to include match patterns

--- popup.js
+++ popup.js
@@ -1,18 +1,17 @@
+/*
+ * This code runs in the popup, when you click the extension
+ * button. Not sure if we even need this. Maybe just load the
+ * page for viewing stats here.
+ */
 var tracker = {
     saySomething: function() {
-      //chrome.runtime.getBackgroundPage( function(currWindow) {
-      //  alert(currWindow);
-        var msg = document.createElement('p');
-        var msgt = document.createTextNode('This is a message?');
-        //var win = document.createTextNode(currWindow.location.host);
-        msg.appendChild(msgt);
-        //msg.appendChild(win);
-        document.body.appendChild(msg);
-      //}
+      var msg = document.createElement('p');
+      var msgt = document.createTextNode('This is a message?');
+      msg.appendChild(msgt);
+      document.body.appendChild(msg);
     }
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
   tracker.saySomething();
 });
