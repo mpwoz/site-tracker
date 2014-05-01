e8ca7755cb71a9c4f3d47230369279e71ec7cf35
mpwoz <martin.woz@gmail.com>
2014-04-30 19:55:23 -0500

    adding pages to local storage

--- js/contentscript.js
+++ js/contentscript.js
@@ -3,6 +3,20 @@ var visited = window.location.hostname;
 console.log(visited);
 
 
-chrome.storage.sync.set({'visitedPages':{pageUrl:visited}}, function () {
-  console.log("Just visited",visited)
+chrome.storage.local.get("visitedPages", function(result) {
+  pages = result["visitedPages"];
+  console.log(pages);
+  pages.push({pageUrl: visited}); // Add the new page
+  chrome.storage.local.set({'visitedPages':pages}, function () {
+    console.log("Just visited",visited)
+  });
+});
+
+
+chrome.storage.local.get("visitedPages", function(items) {
+  console.log("Got from storage:", items);
+});
+
+chrome.storage.local.clear(function() {
+  console.log("Cleared");
 });
